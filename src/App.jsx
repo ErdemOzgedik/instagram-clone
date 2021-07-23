41d6e97fb0addead25af516196b344f325e0f363
ErdemOzgedik <erdemozgedik@gmail.com>
2021-07-24 00:13:45 +0300

    added firebase user auth

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,8 @@
-import { useEffect } from "react";
-import { useState } from "react";
+import { useState, useEffect } from "react";
 import "./App.css";
+import Login from "./components/login/Login";
 import Post from "./components/post/Post";
+import Signup from "./components/signup/Signup";
 import { db } from "./firebase.jsx";
 
 function App() {
@@ -26,6 +27,8 @@ function App() {
           src="https://www.instagram.com/static/images/web/mobile_nav_type_logo.png/735145cfe0a4.png"
           alt=""
         ></img>
+        <Login />
+        <Signup />
       </div>
 
       {posts.map((post) => (
